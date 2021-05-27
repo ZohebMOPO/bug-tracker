c87a500cf72801d8c0e8e7498739b226a717579b
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-27 23:32:30 +0530

    Validators Updated

--- server/src/utils/validators.ts
+++ server/src/utils/validators.ts
@@ -16,14 +16,47 @@ interface BugErrors {
 
 export const registerValidator = (username: string, password: string) => {
   const errors: AuthErrors = {};
+
+  if (
+    !username ||
+    username.trim() === "" ||
+    username.length > 20 ||
+    username.length < 3
+  ) {
+    errors.username =
+      "The username is not following the rules of setting an username";
+  }
+
+  if (!password || password.length < 6) {
+    errors.password = "The passsword is not right";
+  }
+
+  return {
+    errors,
+  };
 };
 
 export const loginValidator = (username: string, password: string) => {
   const errors: AuthErrors = {};
+
+  if (!username || username.trim() === "") {
+    errors.username = "The username is not right";
+  }
+  if (!password) {
+    errors.password = "The password is not right";
+  }
+
+  return {
+    errors,
+  };
 };
 
 export const projectNameError = (name: string) => {
   const errors: ProjectErorrs = {};
+
+  if (!name || name.trim() === "" || name.length > 60) {
+    errors.name = "The name length should be under 60 charecters";
+  }
 };
 
 export const projectMemberError = (members: string) => {
@@ -40,4 +73,21 @@ export const createBugValidator = (
   priority: string
 ) => {
   const errors: BugErrors = {};
+  const priorities = ["high", "middle", "low"];
+
+  if (!title || title.trim() === "" || title.length > 60 || title.length < 3) {
+    errors.title = "The title should be in the limit of 3-60";
+  }
+
+  if (!description || description.trim() === "") {
+    errors.description = "The description is not there";
+  }
+
+  if (!priority || !priorities.includes(priority)) {
+    errors.priority = "No priority given";
+  }
+
+  return {
+    errors,
+  };
 };
