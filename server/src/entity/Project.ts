3ba5e02a14c256dfd1d0722ae4746aa6af508764
Zoheb Alli Khan <understandingzak@gmail.com>
2021-06-02 09:31:46 +0530

    Project Updated

--- server/src/entity/Project.ts
+++ server/src/entity/Project.ts
@@ -1,10 +1,20 @@
-import { Entity, Column } from "typeorm";
+import { Entity, Column, ManyToOne, JoinColumn, OneToMany } from "typeorm";
 import BaseEntity from "./BaseModel";
+import { User } from "./User";
 
 @Entity({ name: "projects" })
 export class Projects extends BaseEntity {
   @Column({ type: "varchar", length: "60" })
   name: string;
+  @ManyToOne(() => User, (user) => user)
+  @JoinColumn({ name: "createdById" })
+  createdBy: User;
   @Column({ type: "varchar" })
-  createdBy: string;
+  createdById: string;
+  @OneToMany(() => Member, (member) => member.project)
+  @JoinColumn()
+  members: Member[];
+  @OneToMany(() => Bug, (bug) => bug.project)
+  @JoinColumn()
+  bugs: Bug[];
 }
